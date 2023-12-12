8263ef3e7a566f8b76cdee954d9179d1d5cc345c
MasamiTuzukibashi <k022g0080@g.neec.ac.jp>
2023-12-12 10:37:10 +0900

    change a bit

--- vector.h
+++ vector.h
@@ -745,23 +745,22 @@ Matrix4x4 MakeAffineMatrix(Vector3 scale, Vector3 rotate, Vector3 translate) {
 //透視投影行列
 Matrix4x4 MakePerspectiveFovMatrix(float fovY, float aspectRatio, float nearClip, float farClip) {
 	Matrix4x4 result;
-	result.m[0][0] = 1 / aspectRatio * 1 / std::tan(fovY / 2);
+	result.m[0][0] = (1.0f / aspectRatio) * (1.0f / std::tan(fovY / 2));
 	result.m[0][1] = 0;
 	result.m[0][2] = 0;
 	result.m[0][3] = 0;
 	result.m[1][0] = 0;
-	result.m[1][1] = 1 / std::tan(fovY / 2);
+	result.m[1][1] = (1.0f / std::tan(fovY / 2));
 	result.m[1][2] = 0;
 	result.m[1][3] = 0;
 	result.m[2][0] = 0;
 	result.m[2][1] = 0;
 	result.m[2][2] = farClip / (farClip - nearClip);
-	result.m[2][3] = 1;
+	result.m[2][3] = 1.0f;
 	result.m[3][0] = 0;
 	result.m[3][1] = 0;
-	result.m[3][2] = -nearClip * farClip / (farClip - nearClip);
+	result.m[3][2] = (-nearClip * farClip) / (farClip - nearClip);
 	result.m[3][3] = 0;
-
 	return result;
 }
 //正射影・viewProjection
